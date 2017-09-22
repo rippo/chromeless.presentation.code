061d9b73a53843a32f16c177ecf82b4a9251c341
Richard Wilde <richard@wildesoft.net>
2017-09-22 09:21:22 +0100

    Finish off the page object pattern tests (mimics the casper ones)

--- Tests/page.object.pattern/account.search.page.js
+++ Tests/page.object.pattern/account.search.page.js
@@ -6,10 +6,20 @@ var AccountSearchPage = function (chromeless, options) {
     this.searchLocator = 'input[id="Query"]'
     this.submitButtonLocator = 'form input[type="submit"]'
     this.inputWithValidationErrorLocator = 'input.input-validation-error'
-    this.resultsTableLocator = 'table[id="results"]'
     this.page = '/account/search/'
+    
+    this.resultsTableId = 'results'
+    this.resultsTableLocator = 'table[id="' + this.resultsTableId + '"]'
+
+    this.noResultsTableId = 'noResults'
+    this.noResultsTableLocator = 'div[id="' + this.noResultsTableId + '"]'
 };
 
+AccountSearchPage.prototype.visit = async function (id) {
+    await this.chromeless
+        .goto(this.options.url + this.page + id)
+        .setViewport({ width: 800, height: 600, scale: 1 })
+}
 
 AccountSearchPage.prototype.checkWeAreOnTheSearchPage = async function (id) {
     const href = await this.chromeless
@@ -21,7 +31,9 @@ AccountSearchPage.prototype.checkWeAreOnTheSearchPage = async function (id) {
 
 AccountSearchPage.prototype.fillInSearchBox = async function (query) {
     await this.chromeless
+        .clearInput(this.searchLocator)
         .type(query, this.searchLocator)
+        .click(this.submitButtonLocator)
 }
 
 
@@ -30,17 +42,27 @@ AccountSearchPage.prototype.submitTheForm = async function () {
         .click(this.submitButtonLocator)
 }
 
-AccountSearchPage.prototype.checkResultsLength = async function (expectedLength) {
-    return await this.chromeless
+AccountSearchPage.prototype.getTableResultsLength = async function () {
+   return await this.chromeless
         .wait(this.resultsTableLocator)
-        .evaluate(() => 
-                document.getElementsByClassName('table')[0].innerHTML)
+        .evaluate(
+            (locator) => { 
+                return document.getElementById(locator)
+                .getElementsByTagName("tbody")[0]
+                .getElementsByTagName("tr").length
+            }
+        , this.resultsTableId)
 }
 
-// document.getElementsByClassName('field-validation-error')[0]
-//     .innerHTML === 'User could not be found'
-// )
-
+AccountSearchPage.prototype.checkNoResultsAlertIsShown = async function () {
+    
+        return await this.chromeless
+        .wait(this.noResultsTableLocator)
+            .evaluate(
+                (locator) => { 
+                    return document.getElementById(locator).innerHTML
+                }, this.noResultsTableId)
+    }
 
 AccountSearchPage.prototype.checkValidationMessageShown = async function () {
     return await this.chromeless
--- Tests/page.object.pattern/test.login.as.test2.js
+++ Tests/page.object.pattern/test.login.as.test2.js
@@ -5,7 +5,7 @@ const AccountLoginPage = require('./account.login.page')
 const AccountSearchPage = require('./account.search.page')
 const options = require('yargs-parser')(process.argv.slice(2))
 
-describe('When logging in as test1@test.com', function () {
+describe('When logging in as test2@test.com', function () {
 
     this.timeout(10000)
     var chromeless = new Chromeless(/*{debug: true}*/)
@@ -13,32 +13,26 @@ describe('When logging in as test1@test.com', function () {
     var loginPage = new AccountLoginPage(chromeless, options)
     var searchPage = new AccountSearchPage(chromeless, options)
 
-    it('login as test1@test1.com', async function() {
-        await loginPage.fullLogin('test1@test.com', 'abc')
+    it('login as test2@test.com', async function() {
+        await loginPage.fullLogin('test2@test.com', 'abc')
     })
 
     it('check we are on the search page', async function() {
-        const ok = await searchPage.checkWeAreOnTheSearchPage("1")
+        const ok = await searchPage.checkWeAreOnTheSearchPage(2)
         expect(ok).to.be.true
     })
 
-    it('check validation is show for a blank search', async function() {
-        await searchPage.submitTheForm()
-        const ok = await searchPage.checkValidationMessageShown()
-        expect(ok).to.be.true
-    })
-
-    it ('when we do a search for \'a\' then return 3 rows', async function() {
+    it ('when we do a search for \'a\' then return 4 rows', async function() {
         await searchPage.fillInSearchBox('a')
         const resultsCount = await searchPage.getTableResultsLength()
-        expect(resultsCount).to.equal(3)
+        expect(resultsCount).to.equal(4)
     })
 
-    it ('when we do a search for \'b\' then return 1 rows', async function() {
-        await searchPage.visit(1)
+    it ('when we do a search for \'b\' then show no results could be found', async function() {
+        await searchPage.visit(2)
         await searchPage.fillInSearchBox('b')
-        const resultsCount = await searchPage.getTableResultsLength()
-        expect(resultsCount).to.equal(1)
+        const noResultsShown = await searchPage.checkNoResultsAlertIsShown()
+        expect(noResultsShown).contains("Sorry no results could be found")
     })
 
    
